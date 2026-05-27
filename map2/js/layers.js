c00ef5e9d565dc7177d9abe9ba8995a2311abc00
Mirian Valk <93868269+hallhuljes@users.noreply.github.com>
2026-05-27 15:32:06 +0300

    Add files via upload

---
+++ map2/js/layers.js
@@ -0,0 +1,64 @@
+const wmsLayers = [
+  {
+    id: "rescue_commands",
+    title: {
+      en: "Estonian rescue commands",
+      ee: "Eesti päästekomandod",
+    },
+    url: "https://landscape-geoinformatics.ut.ee/geoserver/pa2023/wms?",
+    layers: "komandod_prognoosimudelile_db",
+    type: "wms",
+    version: "1.1.1",
+    format: "image/png",
+    transparent: true,
+    projection: "EPSG:3301",
+    zIndex: 300,
+  },
+  {
+    id: "5_minute_areas",
+    title: {
+      en: "5 minute coverage area",
+      ee: "5 minutiga kaetav ala",
+    },
+    url: "https://landscape-geoinformatics.ut.ee/geoserver/pa2023/wms?",
+    layers: "rpk_5min_ala_db",
+    type: "wms",
+    version: "1.1.1",
+    format: "image/png",
+    transparent: true,
+    projection: "EPSG:3301",
+    zIndex: 290,
+  },
+  {
+    id: "10_minute_areas",
+    title: {
+      en: "10 minute coverage area",
+      ee: "10 minutiga kaetav ala",
+    },
+    url: "https://landscape-geoinformatics.ut.ee/geoserver/pa2023/wms?",
+    layers: "rpk_10min_ala_db",
+    type: "wms",
+    version: "1.1.1",
+    format: "image/png",
+    transparent: true,
+    projection: "EPSG:3301",
+    zIndex: 280,
+  },
+  {
+    id: "varjumiskohad",
+    title: {
+      en: "Public hiding places",
+      ee: "Avalikud varjumiskohad",
+    },
+    url: "https://landscape-geoinformatics.ut.ee/geoserver/pa2023/wms?",
+    layers: "varjumiskohad_2024_03_15_db",
+    type: "wms",
+    version: "1.1.1",
+    format: "image/png",
+    transparent: true,
+    projection: "EPSG:3301",
+    zIndex: 310,
+  },
+]
+
+export { wmsLayers }
